487996bcdba626afc5c2c8b21dae3b3cc711792b
Alberto Hernandez <alberto.hernandez@controlintegral.mx>
2019-02-07 17:02:35 -0600

    Avances inicio de sesion

--- public/js/app.js
+++ public/js/app.js
@@ -32764,8 +32764,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\syv\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\syv\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\seebsolutions\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\seebsolutions\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
